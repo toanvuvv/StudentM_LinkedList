8db533782ff503ae6794333332ab6774b896003b
vu duc toan <toanv_mac@MacBook-Pro-cua-vu.local>
2023-12-29 00:15:16 +0700

    toan complete

--- utils/convert_pass.py
+++ utils/convert_pass.py
@@ -1,4 +1,33 @@
-from encryption_util import encrypt_data, decrypt_data
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+def load_key():
+    """Load the encryption key from the .env file."""
+    key = os.getenv("ENCRYPTION_KEY")
+    if not key:
+        raise ValueError("No ENCRYPTION_KEY found in .env file")
+    return key
+
+# Generate a key and instantiate a Fernet object
+key = load_key()
+cipher_suite = Fernet(key)
+
+def encrypt_data(data):
+    """Encrypt data."""
+    if isinstance(data, str):
+        data = data.encode()
+    encrypted_data = cipher_suite.encrypt(data)
+    return encrypted_data
+
+def decrypt_data(encrypted_data):
+    """Decrypt data."""
+    decrypted_data = cipher_suite.decrypt(encrypted_data)
+    return decrypted_data.decode()
+
 import base64
 
 def encrypt_and_encode(password):
@@ -16,7 +45,7 @@ def decode_and_decrypt(encoded_password):
     return decrypted_password
 
 # Mật khẩu ban đầu
-original_password = "admin"
+original_password = "phuongthanh"
 
 # Mã hóa và mã hóa Base64
 encoded_password = encrypt_and_encode(original_password)
